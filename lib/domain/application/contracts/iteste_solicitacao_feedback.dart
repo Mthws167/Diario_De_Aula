babb92594b663d9548469976071cd8d016b8acc9
andrelarrosa <andre.larrosa@outlook.com>
2023-08-17 20:01:45 -0300

    teste

--- lib/domain/application/contracts/iteste_solicitacao_feedback.dart
+++
@@ -1,5 +0,0 @@
-import 'package:diario_aula/domain/dto/agendamento_aula_dto.dart';
-
-abstract class ITesteSolicitacaoFeedback {
-  bool validarAgendamento(SolicitacaoFeedbackDTO agendamentoAulaDTO);
-}
